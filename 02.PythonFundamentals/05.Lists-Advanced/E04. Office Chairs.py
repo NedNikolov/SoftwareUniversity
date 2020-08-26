2e4257835171deebc6c83cb4b359ab89a4f719e9
NedNikolov <NedNikolov@users.noreply.github.com>
2020-08-26 12:35:07 +0300

    added solution

--- 02.PythonFundamentals/05.Lists-Advanced/E04. Office Chairs.py
+++ 02.PythonFundamentals/05.Lists-Advanced/E04. Office Chairs.py
@@ -1,1 +1,18 @@
+def room_chairs(rooms):
+    boolean = True
+    free_chairs = 0
+    for i in range(1, rooms + 1):
+        command = input().split(' ')
+        chairs = len(command[0])
+        taken = int(command[1])
+        if chairs < taken:
+            print(f"{taken - chairs} more chairs needed in room {i}")
+            boolean = False
+        else:
+            free_chairs += (chairs - taken)
+    if boolean:
+        print(f"Game On, {free_chairs} free chairs left")
 
+
+total_rooms = int(input())
+room_chairs(total_rooms)
