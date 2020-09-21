126d1ed985c3470e1496017a52e3aaad95f5a8b6
NedNikolov <NedNikolov@users.noreply.github.com>
2020-09-21 17:05:24 +0300

    add solution

--- 02.PythonFundamentals/06.Classes-and-Objects/E03. Catalogue.py
+++ 02.PythonFundamentals/06.Classes-and-Objects/E03. Catalogue.py
@@ -1,5 +1,4 @@
-class Calatogue:
-    list = []
+class Catalogue:
 
     def __init__(self, name):
         self.name = name
@@ -9,12 +8,17 @@ class Calatogue:
         self.products.append(product)
 
     def get_by_letter(self, first_letter):
-        for prod in self.products:
-            word = self.products[prod]
-            if first_letter == chr(word[0]):
-                list.append(word)
-            return list
+        return [item for item in self.products if item[0] == first_letter]
 
     def __repr__(self):
-        print(f"Items in the {self.name} catalogue:\n")
-        return f" "
+        return f"Items in the {self.name} catalogue:\n" + '\n'.join(sorted(self.products))
+
+
+catalogue = Catalogue("Furniture")
+catalogue.add_product("Sofa")
+catalogue.add_product("Mirror")
+catalogue.add_product("Desk")
+catalogue.add_product("Chair")
+catalogue.add_product("Carpet")
+print(catalogue.get_by_letter("C"))
+print(catalogue)
