177c33ec5669394c23978626bf800e8bf14bd744
NedNikolov <NedNikolov@users.noreply.github.com>
2021-09-14 18:00:17 +0300

    project added

---
+++ 03.Python Advanced/03.ListsAsStacksAndQueues/04.WaterDispenser.py
@@ -0,0 +1,26 @@
+from collections import deque
+names = deque()
+litters = int(input())
+
+while True:
+    name = input()
+    if name != 'Start':
+        names.append(name)
+    else:
+        break
+
+command = input()
+
+while command != "End":
+    if 'refill' in command:
+        litter2 = list(command.split(" "))
+        litters += int(litter2[1])
+    else:
+        litter = int(command)
+        if litter <= litters:
+            litters -= litter
+            print(f"{names.popleft()} got water")
+        else:
+            print(f"{names.popleft()} must wait")
+    command = input()
+print(f"{litters} litters left")
