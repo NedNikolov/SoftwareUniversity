0892a2ced575a4c9b1f249561bfbdf2a011c9ed5
NedNikolov <NedNikolov@users.noreply.github.com>
2021-10-04 17:25:51 +0300

    added

---
+++ 03.Python Advanced/07.StacksQueuesTuplesAndSetsExcercise/04.Honey.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+bees = deque([int(x) for x in input().split()])
+nectar = [int(x) for x in input().split()]
+signs = deque(input().split())
+total_honey = 0
+
+while bees and nectar:
+    curr_bee = bees.popleft()
+    curr_nectar = nectar.pop()
+
+    if curr_nectar >= curr_bee:
+        sign = signs.popleft()
+        if sign == '+':
+            total_honey += abs(curr_bee + curr_nectar)
+        elif sign == '-':
+            total_honey += abs(curr_bee - curr_nectar)
+        elif sign == '*':
+            total_honey += abs(curr_bee * curr_nectar)
+        elif sign == '/':
+            if curr_nectar > 0:
+                total_honey += abs(curr_bee // curr_nectar)
+    else:
+        bees.appendleft(curr_bee)
+
+print(f"Total honey made: {total_honey}")
+
+if bees:
+    print(f"Bees left: {', '.join([str(x) for x in bees])}")
+if nectar:
+    print(f"Nectar left: {', '.join([str(x) for x in nectar])}")
---
+++ 03.Python Advanced/07.StacksQueuesTuplesAndSetsExcercise/06.PaintColors.py
@@ -0,0 +1,7 @@
+from collections import deque
+
+substrings = deque(input().split())
+
+main_colors = {"red", "yellow",  "blue"}
+secondary_colors = {"orange", "purple",  "green"}
+
