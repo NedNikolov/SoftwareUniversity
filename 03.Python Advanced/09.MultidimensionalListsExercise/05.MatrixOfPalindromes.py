345d85772905d2a8bca6589f9d833c7d1a2b92bf
NedNikolov <NedNikolov@users.noreply.github.com>
2021-10-11 01:06:24 +0300

    added

--- 03.Python Advanced/09.MultidimensionalListsExercise/05.MatrixOfPalindromes.py
+++ 03.Python Advanced/09.MultidimensionalListsExercise/05.MatrixOfPalindromes.py
@@ -1,19 +1,7 @@
 rows, cols = [int(x) for x in input().split(' ')]
 matrix = [[0 for i in range(cols) ] for j in range(rows)]
-a = "a"
-b = "a"
-c = "a"
 
 for row in range(rows):
     for col in range(cols):
-        result = b + a + b
-        matrix[row][col] = result
-        a = chr(ord(a) + 1)
-    a = chr(ord(c) + 1)
-    c = chr(ord(c) + 1)
-    b = chr(ord(b) + 1)
-
-for i in range(len(matrix)):
-    for j in range(len(matrix[i])):
-        print(matrix[i][j], end=" ")
+        print(f'{chr(97 + row)}{chr(97 + row + col)}{chr(97 + row)}', end=' ')
     print()
--- 03.Python Advanced/09.MultidimensionalListsExercise/06.MatrixOfShuffling.py
+++ 03.Python Advanced/09.MultidimensionalListsExercise/06.MatrixOfShuffling.py
@@ -5,18 +5,18 @@ for rows in range(row):
     cols = [x for x in input().split(' ')]
     matrix.append(cols)
 
-command = input().split(' ')
-while command[0] != "END":
-    if command[0] != 'swap' or int(command[1]) > row or int(command[2]) > col or \
+
+while True:
+    command = input().split(' ')
+    if command[0] == "END":
+        break
+    if command[0] != 'swap' or len(command) != 5 or int(command[1]) > row or int(command[2]) > col or \
             int(command[3]) > row or int(command[4]) > col:
         print("Invalid input!")
     else:
-        swap_one = matrix[int(command[1])][int(command[2])]
-        swap_two = matrix[int(command[3])][int(command[4])]
-        matrix[int(command[1])][int(command[2])] = swap_two
-        matrix[int(command[3])][int(command[4])] = swap_one
+        matrix[int(command[1])][int(command[2])], matrix[int(command[3])][int(command[4])] \
+            = matrix[int(command[3])][int(command[4])], matrix[int(command[1])][int(command[2])]
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
                 print(matrix[i][j], end=" ")
             print()
-    command = input().split(' ')
