6354b9982ac1c9caa7577c71882492075294ccc2
NedNikolov <NedNikolov@users.noreply.github.com>
2024-02-05 11:12:40 +0200

    a
    a

--- 01.ProgrammingBasics/PercentageCalculator.py
+++ 01.ProgrammingBasics/PercentageCalculator.py
@@ -1,37 +1,40 @@
 print(f"Здравейте,")
 print(f"Това е процентов калкулатор.")
-print(f"Изберете 1 за: Колко е определена стойност в процент от цялата стойност")
-print(f"Изберете 2 за: Колко процента е определена стойност от цялата стойност")
-print(f"Изберете 3 за: Колко са 100% от определена стойност")
+print(f"Изберете валута:", end="")
+currnency_input = input()
+
+print(f"Изберете 1 за: Колко е определена стойност в процент от цялата стойност.")
+print(f"Изберете 2 за: Колко процента е определена стойност от цялата стойност.")
+print(f"Изберете 3 за: Колко процента са от определена стойност.")
 command = int(input())
 
 while command != 4:
     if command == 1:
-        print(f"Колко е определена стойност в процент от цялата стойност")
-        print(f"Percentage:", end="")
+        print(f"Колко е определена стойност в процент от цялата стойност.")
+        print(f"Процент:", end="")
         percent = int(input())
-        print(f"Total:", end="")
+        print(f"Обща стойност:", end="")
         total = int(input())
 
         result = (total * percent) / 100
-        print(f"{percent}% от {total} са {result:.2f}")
+        print(f"{percent}% от {total}{currnency_input}. са {result:.2f}{currnency_input}")
     elif command == 2:
-        print(f"Колко процента е определена стойност от цялата стойност")
-        print(f"Value:", end="")
+        print(f"Колко процента е определена стойност от цялата стойност.")
+        print(f"Търсена стойност:", end="")
         value = int(input())
-        print(f"Total:", end="")
+        print(f"Обща стойност:", end="")
         total = int(input())
 
         result = 100 / (total / value)
-        print(f"{value}% от {total} са {result:.2f}%")
+        print(f"{value}{currnency_input} от {total}{currnency_input} са {result:.2f}%")
     else:
-        print(f"Колко са 100% от определена стойност")
-        print(f"Value:", end="")
+        print(f"Колко процента са от определена стойност.")
+        print(f"Обща стойност:", end="")
         value = int(input())
-        print(f"Percentage:", end="")
+        print(f"Процент:", end="")
         percentage = int(input())
 
-        result = (100 / percentage) * value
-        print(f"100% от {value} са {result}")
-    print(f"Ако желаете да излезете изберете 4, ако желаете да калкулирате отново изберете следователно 1, 2 или 3")
+        result = (percentage / 100) * value
+        print(f"{percentage}% от {value}{currnency_input} са {result}{currnency_input}")
+    print(f"Ако желаете да излезете изберете 4, ако желаете да калкулирате отново изберете следователно 1, 2 или 3.")
     command = int(input())
